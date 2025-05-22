7defb9883d66ed0d6f8f68ea30efb4e956a8a625
kakaprince46 <kakaprince46@gmail.com>
2025-05-22 16:10:31 +0100

    Your descriptive commit message here

--- app.py
+++ app.py
@@ -6,12 +6,14 @@ import pandas as pd
 import firebase_admin
 from firebase_admin import credentials, firestore
 from dotenv import load_dotenv
+from flask_cors import CORS # Import CORS
 
 # Load environment variables
 load_dotenv()
 
 # Initialize Flask app
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 # === Firebase Initialization ===
 def initialize_firebase():
@@ -124,3 +126,4 @@ def predict():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+    
